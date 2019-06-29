05742653f5ba5a69b1586979fe68b37e8f220c92
jsn <jjh_0106@naver.com>
2019-06-30 00:03:28 +0900

    db세팅, User table 정의

---
+++ back/models/user.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        nickname: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+        },
+        userId: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+    },
+    {
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+    });
+
+    // 관계를 정의
+    User.associate = (db) => {
+        db.User.hasMany(db.Post);
+        db.User.hasMany(db.Comment);
+    };
+
+    return User;
+};
